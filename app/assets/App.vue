602bb00e357deeef3b76c235329e56efd9ac7683
Dawid Lewandowski <dawid.lewandowski@sunzinet.com>
2022-08-15 15:59:22 +0200

    Init vue app inside symfony application

---
+++ app/assets/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Hello from Vue js ! Hello {{ name }}</h1>
+  <h2 @click="onNameChange('name 1')">Dawid</h2>
+  <h2 @click="onNameChange('name 2')">Tomek</h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'name 1'
+    }
+  },
+
+  methods: {
+    onNameChange(name) {
+      this.name = name;
+    }
+  }
+}
+</script>
