0e17e127d15d4fdcbe26291682fef98679ead495
Dawid Lewandowski <l33wy@wp.pl>
2022-08-28 16:17:30 +0200

    Unmount vue from index page + enabled files reloading for wepack

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,7 +1,12 @@
-import { createApp } from 'vue';
-import App from './vue/components/App.vue';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
 import './bootstrap';
-
-const app = createApp(App);
-
-app.mount('#app');
