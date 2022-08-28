ebd5ed92de5a68ddbb62cab4a5e368947ffa68e8
Dawid Lewandowski <l33wy@wp.pl>
2022-08-28 19:18:53 +0200

    init bootstrap + added mock side bar

--- app/src/Controller/FrontendController.php
+++ app/src/Controller/FrontendController.php
@@ -11,6 +11,6 @@ class FrontendController extends AbstractController
     #[Route('/', name: 'front.index')]
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('page/home/index.html.twig');
     }
 }
