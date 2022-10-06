651c32ec375a75f6da24b7eb610243c8f9b1eacc
Dawid Lewandowski <l33wy@wp.pl>
2022-10-06 19:31:43 +0200

    Styling sidebar

--- app/src/Controller/Account/AccountHomeController.php
+++ app/src/Controller/Account/AccountHomeController.php
@@ -3,17 +3,21 @@
 namespace App\Controller\Account;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountHomeController extends AbstractController
 {
     #[Route('/', name: 'account.index')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
 
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        return $this->render('page/account/home/index.html.twig');
+        return $this->render('page/account/home/index.html.twig', [
+            'user' => ($this->getUser())->eraseCredentials(),
+            'routeName' => $request->get('_route')
+        ]);
     }
 }
